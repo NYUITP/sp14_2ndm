a79f26780144123f54e2dfefb788132128789c9f
Sam <samarth.mehta@nyu.edu>
2014-02-23 19:25:10 -0500

    Initialze repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sp142ndm::Application.config.secret_key_base = '1b6a32c662e72d25ac1f1c760b86e6b28e9dbbdf88883d8821f836f7bf73604670f19f15cb982cd9821340aaf3b851de192ae5ad43e82041e2aee2262c38110c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sp142ndm::Application.config.session_store :cookie_store, key: '_sp14_2ndm_session'

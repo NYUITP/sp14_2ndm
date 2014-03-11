dc9125a8d4acdd1f399c8df15dd5d292cbe5a885
Sam <samarth.mehta@nyu.edu>
2014-03-10 22:55:36 -0400

    make ticker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,23 +4,18 @@ require 'json'
 class HomeController < ApplicationController
   def index
   	if user_signed_in?
-		base_url = "https://www.bitstamp.net/api/ticker/"
-		response = RestClient.get base_url
-		data = JSON.load response
-		@bitstamp = data["last"]
-		base_url = "https://coinbase.com/api/v1/prices/buy"
-		response = RestClient.get base_url
-		data = JSON.load response
-		@coinbase = data["amount"]
 		@holdings = Holding.all
 		base_url = "https://www.bitstamp.net/api/transactions/"
 		response = RestClient.get base_url
 		data = JSON.load response
 		@bitstamp_his = data
 		@total = 0
+		@coinbase = Coinbase::Client.new('9MB2hsDaSXvbevZ4', 'Yakw1TObmQrL2k4OMGcCVZqpdNLsPO2S')
+		@cb2 = @coinbase.buy_price(1).format
 		Holding.all.where(:user_id => current_user.id).each do |holding|
 			@total = @total + holding.balance
 		end
 	end
   end
+  
 end

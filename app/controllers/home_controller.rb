200f73f8d735d13c9fc97296774fc47701fa6403
subodhtiwaskar <subodhtiwaskar@gmail.com>
2014-04-21 19:36:00 -0400

    chnages to prices service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -35,15 +35,15 @@ class HomeController < ApplicationController
 
   def history
  	if user_signed_in?
-	#      base_url = "https://www.bitstamp.net/api/ticker"
+	  #    base_url = "https://www.bitstamp.net/api/ticker"
 		#response = RestClient.get base_url
 		#data = JSON.load response
 	#	@bitstamp_his = 500
 	#	@coinbase = Coinbase::Client.new('9MB2hsDaSXvbevZ4', 'Yakw1TObmQrL2k4OMGcCVZqpdNLsPO2S')
-	 # 	@coinbase_his = @coinbase.buy_price(1).to_f
+	# 	@coinbase_his = @coinbase.buy_price(1).to_f
 	#	x = PriceHistory.new
-	 # 	x.Bitstamp = @bitstamp_his
-	  #	x.Coinbase= @coinbase_his
+	# 	x.Bitstamp = @bitstamp_his
+	 # 	x.Coinbase= @coinbase_his
 	#	x.save
 		response = PriceHistory.all		
 		render :json => response

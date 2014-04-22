205ddb11ccf95e13e2aba6f660dfe0685a09854a
Sam <samarth.mehta@nyu.edu>
2014-04-22 05:38:32 -0400

    edits to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -40,38 +40,38 @@ class HomeController < ApplicationController
 	  	render :json => reply
   end
 
-  def history
- 	if user_signed_in?
+  #def history
+ 	#if user_signed_in?
 	        #base_url = "https://www.bitstamp.net/api/ticker"
 		#response = RestClient.get base_url
 		#data = JSON.load response
 		#@bitstamp_his = 500
-		@bitstamp_his = Bitstamp::ticker.last.to_f
-		@coinbase = Coinbase::Client.new('9MB2hsDaSXvbevZ4', 'Yakw1TObmQrL2k4OMGcCVZqpdNLsPO2S')
-	  	@coinbase_his = @coinbase.buy_price(1).to_f
-		x = PriceHistory.new
-	  	x.bitstamp = @bitstamp_his
-	  	x.coinbase= @coinbase_his
-		x.save
-		response = PriceHistory.all		
-		render :json => response
+	#	@bitstamp_his = Bitstamp::ticker.last.to_f
+	#	@coinbase = Coinbase::Client.new('9MB2hsDaSXvbevZ4', 'Yakw1TObmQrL2k4OMGcCVZqpdNLsPO2S')
+	 # 	@coinbase_his = @coinbase.buy_price(1).to_f
+	#	x = PriceHistory.new
+	 # 	x.bitstamp = @bitstamp_his
+	  #	x.coinbase= @coinbase_his
+		#x.save
+		#response = PriceHistory.all		
+		#render :json => response
 
 		
-	end
-  end 
+	#end
+  #end 
 
   def history
  	if user_signed_in?
-	  #    base_url = "https://www.bitstamp.net/api/ticker"
+	     #base_url = "https://www.bitstamp.net/api/ticker"
 		#response = RestClient.get base_url
 		#data = JSON.load response
-	#	@bitstamp_his = 500
-	#	@coinbase = Coinbase::Client.new('9MB2hsDaSXvbevZ4', 'Yakw1TObmQrL2k4OMGcCVZqpdNLsPO2S')
-	# 	@coinbase_his = @coinbase.buy_price(1).to_f
-	#	x = PriceHistory.new
-	# 	x.Bitstamp = @bitstamp_his
-	 # 	x.Coinbase= @coinbase_his
-	#	x.save
+		#@bitstamp_his = data["last"].to_f
+		#@coinbase = Coinbase::Client.new('9MB2hsDaSXvbevZ4', 'Yakw1TObmQrL2k4OMGcCVZqpdNLsPO2S')
+	 	#@coinbase_his = @coinbase.buy_price(1).to_f
+		#x = PriceHistory.new
+	 	#x.Bitstamp = @bitstamp_his
+	  	#x.Coinbase= @coinbase_his
+		#x.save
 		response = PriceHistory.all		
 		render :json => response
 
